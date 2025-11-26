12593021abf46b1f1cb52492cad2e1fbcad983e1
Dan <31831587+de0015@users.noreply.github.com>
2025-11-26 14:09:09 -0500

    Update SMOLVLM.py

--- SMOLVLM.py
+++ SMOLVLM.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# /// script
+/// script
  dependencies = [
      "trl",
      "torch",
@@ -21,7 +21,7 @@
      "trackio",
      "kernels",
  ]
-# ///
+///
 
 """
 # Full training
